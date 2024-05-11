b57d4f161d3e18cf10d2d03bb4a35b0981f9715d
Mostafa Mansour <85360001+MostafaAMansour@users.noreply.github.com>
2024-05-11 19:13:53 +0300

    Add files via upload

---
+++ Drivers/LIB/BIT_MATH.h
@@ -0,0 +1,17 @@
+/*
+ * BIT_MATH.h
+ *
+ *  Created on: May 17, 2022
+ *      Author: Mostafa Abdallah
+ */
+
+#ifndef LIB_BIT_MATH_H_
+#define LIB_BIT_MATH_H_
+
+#define SET_BIT(Reg, bitnum)       (Reg |=1<<bitnum)
+#define CLR_BIT(Reg, bitnum)       (Reg &=~(1<<bitnum))
+#define TOG_BIT(REG, BIT)		   ( (REG) ^=  (1<<(BIT)) )
+#define GET_BIT(Reg, bitnum)       ((Reg>>bitnum)&1)
+#define WRT_BIT(REG, BIT, VAL) ( (REG) = ( ( (REG) & (~(1<<(BIT))) ) | ( (VAL) << (BIT)  ) ) )
+
+#endif /* LIB_BIT_MATH_H_ */
---
+++ Drivers/LIB/STD_TYPES.h
@@ -0,0 +1,27 @@
+/*
+ * STD_TYPES.h
+ *
+ *  Created on: May 17, 2022
+ *      Author: Mostafa Abdallah
+ */
+
+#ifndef LIB_STD_TYPES_H_
+#define LIB_STD_TYPES_H_
+
+typedef unsigned char u8 ;
+typedef unsigned short int u16 ;
+typedef unsigned long int u32 ;
+typedef signed char s8 ;
+typedef signed short int s16 ;
+typedef signed long int s32 ;
+typedef float f32;
+typedef double f64;
+
+typedef enum {
+	STD_NOK = 0,
+	STD_OK  = 1,
+} enuErrorState_t;
+
+#define NULL ((void*)0)
+
+#endif /* LIB_STD_TYPES_H_ */
