ec74acbe70dcf5ae823f62d6afc5c19a8603b499
Mostafa Mansour <85360001+MostafaAMansour@users.noreply.github.com>
2024-05-14 13:20:05 +0300

    Added all Buffer Functions

--- Drivers/HAL/N5110/N5110_int.h
+++ Drivers/HAL/N5110/N5110_int.h
@@ -1,6 +1,7 @@
 #ifndef HAL_N5110_N5110_INT_H_
 #define HAL_N5110_N5110_INT_H
 
+
 typedef enum N5110_STATE_e{
 	N5110_LOW,
 	N5110_HIGH
@@ -16,8 +17,11 @@ void HN5110_vDrawFullImage(u8 *PA_u8Data);
 void HN5110_vDisplayCustomChar(const u8 *PA_u8Data);
 void HN5110_vDisplay2DImage(const u8 PA_u8Data[][7],u8 A_u8length);
 void HN5110_vHome(void);
+//Buffer Functions
 void HN5110_vSetPixel(u8 A_u8Xpos,u8 A_u8Ypos, N5110_STATE_t A_enState);
 void HN5110_vDisplayBuffer(void);
 void HN5110_vClearBuffer(void);
-
+void HN5110_vAddCharBuffer(u8 A_u8Xpos,u8 A_u8Ypos, u8 A_u8Data);
+void HN5110_vAddStringBuffer(u8 A_u8Xpos,u8 A_u8Ypos,u8 *PA_u8String);
+void HN5110_vAddNumberBuffer(u8 A_u8Xpos,u8 A_u8Ypos,s32 A_s32Number);
 #endif /* HAL_N5110_N5110_INT_H */
