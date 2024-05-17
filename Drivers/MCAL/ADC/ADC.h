953ae7bf57632c1eaa85b7b217925d71a35b7a88
emanmohamed354 <85428890+emanmohamed354@users.noreply.github.com>
2024-05-17 18:19:16 +0300

    Added ADC

---
+++ Drivers/MCAL/ADC/ADC.h
@@ -0,0 +1,17 @@
+#ifndef MCAL_ADC_ADC_PRV_H_
+#define MCAL_ADC_ADC_PRV_H_
+
+
+#define ADC_SEQ3_BIT 3
+#define ADC_RCG_BIT 0
+#define ADC_EMUX_BIT 12
+#define ADC_INT_BIT 2
+#define ADC_END_BIT 1
+#define ADC_CHANNEL_VAL 2
+#define ADC_CONTINIOUS_TRIGGER 0xF
+
+void MADC_vInit(void);
+u16 MADC_u16GetVal(void);
+
+
+#endif /* MCAL_ADC_ADC_PRV_H_ */
